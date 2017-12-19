a45005843bf6ca95e24a0422a09b9fe45c81ac89
Michael Myung <michael.h.myung@gmail.com>
2017-12-19 22:05:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,12 +5,17 @@ function BoardMember(name, homeState, training){
 }
 
 BoardMember.prototype.veto = function(){
+<<<<<<< HEAD
   return 'No, I must disagree'
+=======
+  return 'No, I must disagree'  
+>>>>>>> 4a7fc43c953e4225b9df0030b548146f391666cf
 }
 BoardMember.prototype.approve = function(){
   return 'You can do that!'
 }
 BoardMember.prototype.doCharity = function(){
+<<<<<<< HEAD
     return "I like to help people."
 }
 BoardMember.prototype.releasePressStatement = function(){
@@ -18,4 +23,13 @@ BoardMember.prototype.releasePressStatement = function(){
 }
 BoardMember.prototype.sayHi = function(){
     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
+=======
+    return "I like to help people."  
+}
+BoardMember.prototype.releasePressStatement = function(){
+    return "You will see great things from Scuber."  
+}
+BoardMember.prototype.sayHi = function(){
+    return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`  
+>>>>>>> 4a7fc43c953e4225b9df0030b548146f391666cf
 }
